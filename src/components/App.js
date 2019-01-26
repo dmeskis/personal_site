206184749273b0b23d0b28b1808a6178f5c614e7
Mesky <dmeskis@gmail.com>
2019-01-26 12:32:43 -0700

    Reorganized files

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1>
+            Dylan Meskis
+          </h1>
+          <h3>
+            Software Developer
+          </h3>
           <a
             className="App-link"
             href="https://reactjs.org"
