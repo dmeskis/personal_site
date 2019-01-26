c77762b5ec12416bff278e1a20e22a5e57318d05
Mesky <dmeskis@gmail.com>
2019-01-26 15:39:57 -0700

    Style social icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import '../styles/App.css';
-
+import Github from '../images/github-logo.svg';
+import Google from '../images/google-logo.svg';
+import Instagram from '../images/instagram-logo.svg';
+import LinkedIn from '../images/linkedin-logo.svg';
+import Twitter from '../images/twitter-logo.svg';
 class Contact extends Component {
   
   render() {
@@ -11,21 +15,30 @@ class Contact extends Component {
           <h1>
             Contact
           </h1>
-          <h5>
-            Github
-          </h5>
-          <h5>
-            LinkedIn
-          </h5>
-          <h5>
-            Instagram
-          </h5>
-          <h5>
-            Email
-          </h5>
-          <h5>
-            Turing Alumni Page
-          </h5>
+          <div>
+            <h5>
+              Github
+            </h5>
+            <img class='social-icon' src={Github} alt='Github'></img>
+          </div>
+          <div>
+            <h5>
+              LinkedIn
+            </h5>
+            <img class='social-icon' src={LinkedIn} alt='LinkedIn'></img>
+          </div>
+          <div>
+            <h5>
+              Instagram
+            </h5>
+            <img class='social-icon' src={Instagram} alt='Instagram'></img>
+          </div>
+          <div>
+            <h5>
+              Google
+            </h5>
+            <img class='social-icon' src={Google} alt='Google'></img>
+          </div>
           <button
             onClick={() => {
               this.props.navigate("");
